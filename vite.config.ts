15565796c6bbde27b5d65a9760bb51bdba40420e
apple <apple@apples-MacBook-Pro-2.local>
2025-10-19 01:12:05 +0530

    Added Netlify redirects file

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -10,9 +11,25 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  build: {
+    outDir: "dist", // output folder for Netlify
+    chunkSizeWarningLimit: 1000, // increase size warning limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Separate vendor code (improves loading speed)
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
     },
   },
 }));
